821a76deb48e59610d2da7c330340098fcf7ce48
Eliseo Torres <eliseo.torres95@gmail.com>
2016-07-01 11:31:43 -0500

    Update sniff_AP.py

--- sniff_AP.py
+++ sniff_AP.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from scapy.all import *
 
+APs = [] 
+
 def PacketHandler(pkt):
   if pkt.haslayer(Dot11):
     if pkt.type == 0 and pkt.subtype == 8 :
-      print "MAC: %s con SSID: %s " %(pkt.addr2, pkt.info) 
+      if pkt.addr2 not in aPs:
+        APs.append(pkt.addr2)
+        print "MAC: %s con SSID: %s " %(pkt.addr2, pkt.info) 
       
 sniff(iface = "mon0", prn = PacketHandler)
