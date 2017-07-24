5361497ec1bfca34ab8a10dd39150994a539ff00
Eliseo Torres <eliseo.torres95@gmail.com>
2017-07-23 19:31:52 -0500

    Update sniff_AP.py

--- sniff_AP.py
+++ sniff_AP.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+import requests as rq
+
 from scapy.all import *
 
 APs = [] 
+def fabricante(mac):
+    mac = str(mac).replace(":","-")
+    link = "http://api.macvendors.com/"+mac
+    r = rq.get(link)
+    return r.content
 
 def PacketHandler(pkt):
   if pkt.haslayer(Dot11):
     if pkt.type == 0 and pkt.subtype == 8 :
-      if pkt.addr2 not in aPs:
+      if pkt.addr2 not in APs:
         APs.append(pkt.addr2)
-        print "MAC: %s con SSID: %s " %(pkt.addr2, pkt.info) 
+        mac = pkt.addr2
+        fab = fabricante(mac)
+        print "MAC: %s SSID: %s BUILD: %s" %(pkt.addr2, pkt.info,fab)
+   
       
-sniff(iface = "mon0", prn = PacketHandler)
+sniff(iface = "wlan1", prn = PacketHandler)
+
